574ee555938f6e0a9858b11964e5867b49db43dc
c2kaka <c2kaka@126.com>
2019-09-15 18:50:07 +0800

    refactor-use-universal-crud-interface

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,34 +1,51 @@
 module.exports = app => {
   const express = require("express");
-  const router = express.Router();
-  const Category = require("../../models/Category");
+  const router = express.Router({
+    mergeParams: true
+  });
 
-  router.post("/categories", async (req, res) => {
-    const model = await Category.create(req.body);
+  router.post("/", async (req, res) => {
+    const model = await req.model.create(req.body);
     res.send(model);
   });
 
-  router.get("/categories", async (req, res) => {
-    const model = await Category.find().populate('parent').limit(10);
+  router.get("/", async (req, res) => {
+    let queryOptions = {};
+    const modelName = req.model.modelName;
+    if (modelName === "Category") {
+      queryOptions.populate = "parent";
+    }
+    const model = await req.model
+      .find()
+      .setOptions(queryOptions)
+      .limit(100);
     res.send(model);
   });
 
-  router.get("/categories/:id", async (req, res) => {
-    const model = await Category.findById(req.params.id);
+  router.get("/:id", async (req, res) => {
+    const model = await req.model.findById(req.params.id);
     res.send(model);
   });
 
-  router.put("/categories/:id", async (req, res) => {
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body);
+  router.put("/:id", async (req, res) => {
+    const model = await req.model.findByIdAndUpdate(req.params.id, req.body);
     res.send(model);
   });
 
-  router.delete("/categories/:id", async (req, res) => {
-    await Category.findByIdAndDelete(req.params.id);
+  router.delete("/:id", async (req, res) => {
+    await req.model.findByIdAndDelete(req.params.id);
     res.send({
-        success: true
+      success: true
     });
   });
 
-  app.use("/admin/api", router);
+  app.use(
+    "/admin/api/rest/:resource",
+    async (req, res, next) => {
+      const modelName = require("inflection").classify(req.params.resource);
+      req.model = require(`../../models/${modelName}`);
+      next();
+    },
+    router
+  );
 };
