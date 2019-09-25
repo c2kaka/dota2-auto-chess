94e25bf9f1c290f61a6c826f7b062dfe996d6cc3
c2kaka <c2kaka@126.com>
2019-09-25 11:08:33 +0800

    feat(main.vue): add main.vue component
    add main.vue component, finish basic style of topbar and nav

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,20 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav"></div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="top-bar bg-dark2 p-2 d-flex ai-center">
+        <img src="../assets/logo.png" alt="" srcset="" height="30">
+        <div class="flex-1 px-3">
+            <div class="text-white fs-lg">刀塔自走棋</div>
+            <div class="text-grey1 fs-md">运筹帷幄，决胜千里</div>
+        </div>
+        <div>
+            <button type="button" class="button bg-primary ">立即下载</button>
+        </div>
+    </div>
+
+    <div class="bg-primary pt-3 pb-2">
+        <div class="nav d-flex jc-around text-white pb-1">
+            <div class="nav-item active">
+                <router-link to='/' tag='div'>首页</router-link>
+            </div>
+            <div class="nav-item">
+                <router-link to='/' tag='div'>攻略中心</router-link>
+            </div>
+            <div class="nav-item">
+                <router-link to='/' tag='div'>资讯中心</router-link>
+            </div>
+        </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+</style>  
