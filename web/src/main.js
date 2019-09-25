3797e1a579b9c88e315b0b5a90884ac558aad03c
c2kaka <c2kaka@126.com>
2019-09-25 12:09:37 +0800

    feat(home.vue): add home componentt
    add home.vue, finish image swiper

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 
-import './style.scss';
+import './assets/scss/style.scss';
 import router from './router'
+
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
+
 new Vue({
   router,
   render: h => h(App)
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide>
+        <img src="../assets/images/1.jpg" class="w-100" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/images/2.jpg" class="w-100" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/images/3.jpg" class="w-100" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/images/4.jpg" class="w-100" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/images/5.jpg" class="w-100" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination pagination-home" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        loop: true
+      },
+      swiperSlides: [1, 2, 3, 4, 5]
+    };
+  },
+  computed: {
+    
+  },
+  mounted() {
+    
   }
-}
+};
 </script>
+
+<style lang="scss">
+  .swiper-pagination-bullets {
+    .swiper-pagination-bullet{
+      background: white;
+      opacity: 1;
+
+      &.swiper-pagination-bullet-active{
+        background: blue;
+      }
+    }
+  }
+</style>
